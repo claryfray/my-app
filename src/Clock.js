e0edc14d1949e5d44f7de9bdb6491bd2201a1465
claryfray <targaryenceleste@gmail.com>
2021-02-22 17:52:32 +1100

    Added function to calculate other time zones. Included London, UK

--- src/Clock.js
+++ src/Clock.js
@@ -5,9 +5,30 @@ class Clock extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            time: new Date().toLocaleTimeString()
+            time: new Date().toLocaleTimeString(),
+            london: this.calcTime(0)
         };
     }
+
+    calcTime(offset) {
+
+        // create Date object for current location
+        let d = new Date();
+       
+        // convert to msec
+        // add local time zone offset
+        // get UTC time in msec
+        let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+       
+        // create new Date object for different city
+        // using supplied offset
+        let nd = new Date(utc + (3600000*offset));
+       
+        // return time as a string
+        return nd.toLocaleTimeString();
+    
+    }
+
     componentDidMount() {
         this.intervalID = setInterval(
           () => this.tick(),
@@ -19,14 +40,16 @@ class Clock extends React.Component {
     }
     tick() {
         this.setState({
-          time: new Date().toLocaleTimeString()
+          time: new Date().toLocaleTimeString(),
+          london: this.calcTime(0)
         });
     }
     render() {
         return (
-        <p>
-            Sydney, Australia: {this.state.time}.
-        </p>
+            <p>
+                Sydney, Australia: {this.state.time}.<br></br>
+                London, United Kingdom: {this.state.london}.
+            </p>
     );
   }
 }
